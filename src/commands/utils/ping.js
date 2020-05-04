67ce7e88f49462ad33283fd0aef83675b2346473
vinicius augusto alves <viniciusfabrisilk@gmail.com>
2020-05-04 16:47:45 -0300

    some fix

--- src/commands/utils/ping.js
+++ src/commands/utils/ping.js
@@ -1,13 +1,14 @@
 const Command = require('../../lib/structures/Command')
 
 module.exports = class Ping extends Command {
-  constructor(client){
+  constructor(client) {
     super(client)
     this.name = 'ping'
     this.aliases = ['latencia']
+    this.category = 'utils'
   }
 
-  run({ channel }){
+  run({ channel }) {
     channel.send(`Pong! 🏓 ${this.client.ws.ping}`)
   }
 }
