3bdc6a30da4fe44af9469d8d5f23f5ca0bad55d1
vinicius augusto alves <viniciusfabrisilk@gmail.com>
2020-05-04 18:16:54 -0300

    published

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 const AtlasClient = require('./src/AtlasClient')
-const { owners, prefixes, presence, environment, token } = require('./config')
+const config = require('./config')
 
-const Atlas = new AtlasClient({
-  presence: {
-    activity: {
-      name: presence.activity.name || '',
-      type: presence.activity.type || ''
-    }
-  },
-  token,
-  owners,
-  prefixes,
-  environment,
-})
+const Atlas = new AtlasClient(config)
 
 Atlas.start()
