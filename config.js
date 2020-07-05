4862f7ab9850c26963d4bc7a4869ca7ef2cfe06d
Psykka <36780964+Psykka@users.noreply.github.com>
2020-07-05 20:02:50 -0300

    remove unnecessary impots && fix editor config

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  token: process.env.TOKEN,
-  environment: process.env.ENVIRONMENT,
-  owners: process.env.OWNERS.split(','),
-  prefixes: process.env.PREFIXES.split(','),
-  lavalink: JSON.parse(process.env.LAVALINK),
-  presence: JSON.parse(process.env.PRESENCE || '{}')
-}
--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const AtlasClient = require('./src/AtlasClient')
-const config = require('./config')
 
-const Atlas = new AtlasClient(config)
+const Atlas = new AtlasClient({
+  token: process.env.TOKEN,
+  environment: process.env.ENVIRONMENT,
+  owners: process.env.OWNERS.split(','),
+  prefixes: process.env.PREFIXES.split(','),
+  lavalink: JSON.parse(process.env.LAVALINK),
+  presence: JSON.parse(process.env.PRESENCE || '{}')
+})
 
 Atlas.start()
