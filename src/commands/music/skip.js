1c602e9eb452282974eac82ec1887a3105098abd
Psykka <36780964+Psykka@users.noreply.github.com>
2020-07-30 21:49:54 -0300

    skip command

---
+++ src/commands/music/skip.js
@@ -0,0 +1,17 @@
+const Command = require('../../lib/structures/Command')
+
+module.exports = class Pllay extends Command {
+  constructor(client) {
+    super(client)
+    this.name = 'skip'
+    this.aliases = ['s', 'n']
+    this.category = 'music'
+  }
+
+  async run({ message, guild, channel }) {
+    const player = this.client.music.players.get(guild.id)
+    if(!player) return channel.send('Não estou tocando nada no momento!')
+    player.stop()
+    return message.react('👍')
+  }
+}
