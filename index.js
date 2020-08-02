f19bc8e409e704ac96c7184cf7fb77293c192f63
Psykka <36780964+Psykka@users.noreply.github.com>
2020-08-01 21:11:00 -0300

    using arrays parsed in json

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const AtlasClient = require('./src/AtlasClient')
 const Atlas = new AtlasClient({
   token: process.env.TOKEN,
   environment: process.env.ENVIRONMENT,
-  owners: process.env.OWNERS.split(','),
-  prefixes: process.env.PREFIXES.split(','),
+  owners: JSON.parse(process.env.OWNERS),
+  prefixes: JSON.parse(process.env.PREFIXES),
   nodes: JSON.parse(process.env.LAVALINK),
   presence: JSON.parse(process.env.PRESENCE || '{}'),
   logChannel: process.env.LOGCHANNEL
