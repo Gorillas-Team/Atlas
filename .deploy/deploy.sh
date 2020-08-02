20e05b46c3a47b06a974da6aa35c57e4c4e3bab9
Psykka <36780964+Psykka@users.noreply.github.com>
2020-08-01 22:02:14 -0300

    deploy fix

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -7,6 +7,7 @@ ssh-add .deploy/deploy_rsa
 ssh atlas@$SSH_IP <<EOF
   docker stop {$TRAVIS_BRANCH}
   docker rm {$TRAVIS_BRANCH}
+  docker pull ${DOCKER_IMAGE}:${DOCKER_TAG}
   docker run --name={$TRAVIS_BRANCH} --env-file ~/.env/{$TRAVIS_BRANCH}.env --restart unless-stopped -d ${DOCKER_IMAGE}:${DOCKER_TAG}
   docker system prune -a -f
 EOF
