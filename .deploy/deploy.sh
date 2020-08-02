bfcda669ff9967a734df34ae89e17592981c3ef9
Psykka <36780964+Psykka@users.noreply.github.com>
2020-08-01 23:50:49 -0300

    >:^(

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -5,10 +5,10 @@ chmod 600 .deploy/deploy_rsa
 echo -e "Host $SSH_IP\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 ssh-add .deploy/deploy_rsa
 
-ssh atlas@$SSH_IP <<EOF
-  docker stop {$TRAVIS_BRANCH}
-  docker rm {$TRAVIS_BRANCH}
-  docker pull ${DOCKER_IMAGE}:${DOCKER_TAG}
-  docker run --name={$TRAVIS_BRANCH} --env-file ~/.env/{$TRAVIS_BRANCH}.env --restart unless-stopped -d ${DOCKER_IMAGE}:${DOCKER_TAG}
-  docker system prune -a -f
-EOF
+# ssh atlas@$SSH_IP <<EOF
+#   docker stop {$TRAVIS_BRANCH}
+#   docker rm {$TRAVIS_BRANCH}
+#   docker pull ${DOCKER_IMAGE}:${DOCKER_TAG}
+#   docker run --name={$TRAVIS_BRANCH} --env-file ~/.env/{$TRAVIS_BRANCH}.env --restart unless-stopped -d ${DOCKER_IMAGE}:${DOCKER_TAG}
+#   docker system prune -a -f
+# EOF
