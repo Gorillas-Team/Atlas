c8b36ac32f8939009967e4d7d54fb58bd9713316
Psykka <36780964+Psykka@users.noreply.github.com>
2020-08-04 00:30:15 -0300

    messageUpdate event (helps in eval 😉)

---
+++ src/listeners/discord/messageUpdate.js
@@ -0,0 +1,14 @@
+const { Listener } = require('../../lib/structures')
+
+module.exports = class extends Listener {
+  constructor() {
+    super({
+      name: 'messageUpdate'
+    })
+  }
+
+  async run(oldM, newM) {
+      if(oldM.content === newM.content) return
+      this.emit('message', newM)
+  }
+}
