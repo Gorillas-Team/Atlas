8cadbd007c7a10bf8ed45582366def33455dc978
Psykka <36780964+Psykka@users.noreply.github.com>
2020-08-04 00:43:20 -0300

    fix deploy script

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -7,11 +7,11 @@ echo -e "Host $SSH_IP\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 ssh-add .deploy/deploy_rsa
 
 ssh -i .deploy/deploy_rsa atlas@$SSH_IP << EOF
-  docker stop {$TRAVIS_BRANCH}
-  docker rm {$TRAVIS_BRANCH}
-  echo {$DOCKER_PASSWORD} | docker login -u {$DOCKER_USERNAME} --password-stdin
+  docker stop ${TRAVIS_BRANCH}
+  docker rm ${TRAVIS_BRANCH}
+  echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin
   docker pull ${DOCKER_IMAGE}:${DOCKER_TAG}
-  docker run --name={$TRAVIS_BRANCH} --env-file ~/.env/{$TRAVIS_BRANCH}.env --restart unless-stopped -d ${DOCKER_IMAGE}:${DOCKER_TAG}
+  docker run --name=${TRAVIS_BRANCH} --env-file ~/.env/${TRAVIS_BRANCH}.env --restart unless-stopped -d ${DOCKER_IMAGE}:${DOCKER_TAG}
   docker system prune -a -f
 EOF
 
