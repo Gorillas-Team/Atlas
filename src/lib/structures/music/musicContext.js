bd5253bbdfb6cab28371deab9404ec1d5afe9ff5
Psykka <36780964+Psykka@users.noreply.github.com>
2020-08-28 01:29:11 -0300

    uga uga

--- src/lib/structures/music/musicContext.js
+++ src/lib/structures/music/musicContext.js
@@ -6,18 +6,18 @@ module.exports = function musicContext(options) {
   this.conf = options.conf
   const { channel, member, me, client } = options.ctx
 
-  if (this.conf.needsPlayer && !player) return channel.send(':x: | Não estou tocando nada no momento')
+  if (this.conf.needsPlayer && !this.player) return channel.send(':x: | Não estou tocando nada no momento')
 
   if (this.conf.voiceChannelOnly && this.player && (!this.memberChannel || this.memberChannel !== this.voiceChannel)) {
     return channel.send(`:x: | Estou conectado em \`${client.channels.cache.get(this.voiceChannel.id || this.voiceChannel).name}\``)
   }
 
   if (this.conf.djOnly) {
-    if (this.conf.memberTack && (member.id !== this.player.track.requester.id || !player.isDJ(member))) {
+    if (this.conf.memberTrack && (member.id !== this.player.track.requester.id && !player.isDJ(member))) {
       return channel.send(':x: | Somente o DJ e o requester tem permissão para isso')
     }
 
-    if(!player.isDJ(member)) return channel.send(':x: | Somente o DJ tem permissão para isso')
+    if (!this.player.isDJ(member) && !this.conf.memberTrack) return channel.send(':x: | Somente o DJ tem permissão para isso')
   }
 
   if (this.conf.playingOnly && !this.player.playing) return channel.send(':x: | Não estou tocando nada no momento')
