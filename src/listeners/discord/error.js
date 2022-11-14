f9f7bbe69fe52456707f287f044a59042f36d605
Psykka <36780964+Psykka@users.noreply.github.com>
2022-11-14 03:50:27 -0300

    refactor(Listener): remove log channel from error listener

--- src/listeners/discord/error.js
+++ src/listeners/discord/error.js
@@ -9,11 +9,6 @@ export default class extends Listener {
   }
 
   async run(error) {
-    const channel = this.client.channels.cache.get(this.config.logChannel)
-    const hooks = await channel.fetchWebhooks()
-
-    const webhook = hooks.cache.size === 0 ? channel.createWebhook('AtlasHook', { avatar: this.user.displayAvatarURL({ format: 'png' }), reason: 'Log channel'}) : hooks.first()
-
-    webhook.send(inspect(error, { depth: 0 }), { code: 'js' }).catch(e => console.error('can\'t send Error hook:', e))
+    console.error('[ERROR]', error)
   }
 }
