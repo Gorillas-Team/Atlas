fe64428fdc0d34968fa76817b6673d0eec924221
Psykka <36780964+Psykka@users.noreply.github.com>
2022-11-14 04:16:59 -0300

    refactor(Listener): voiceStateUpdate

--- src/listeners/discord/voiceStateUpdate.js
+++ src/listeners/discord/voiceStateUpdate.js
@@ -12,21 +12,16 @@ export default class extends Listener {
 
     if (!player) return
 
-    const channel = await VSNew.guild.members.fetch(VSNew.id).voice.channel
-
-    if (VSNew.channelID !== channel.id && this.user.id === VSNew.id) {
-      player.updateChannel(VSNew.channelID)
-      // i dont know if this is needed
-      // channel = this.channels.cache.get(VSNew.channelID)
-    }
-
-    if (channel.members.size < 2) {
-      player.channelEmpty = true
-      player.execTimeout()
-    } else {
-      player.channelEmpty = false
-      if (!player.playing) return player.execTimeout()
-      player.execClearTimeout()
+    if (player.voiceChannel.id === VSOld.channelID) {
+      if (VSOld.channel.members.size === 1) {
+        player.channelEmpty = true
+        player.execTimeout()
+      }
+    } else if (player.voiceChannel.id === VSNew.channelID) {
+      if (VSNew.channel.members.size > 1) {
+        player.channelEmpty = false
+        player.execClearTimeout()
+      }
     }
   }
 }
