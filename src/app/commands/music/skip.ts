b1b4e6e81b135c5590d1d022ac5e4642fd0d0a1b
Miguel Moraes <ehomigas@gmail.com>
2025-05-14 00:24:50 -0300

    fix: skip can only be executed in a voice channel

--- src/app/commands/music/skip.ts
+++ src/app/commands/music/skip.ts
@@ -1,6 +1,7 @@
 import { BaseDiscordCommand, CommandContext } from '@/shared/discord/BaseDiscordCommand.js'
 import { Atlas } from '@/app/Atlas.js'
 import { SlashCommandBuilder } from 'discord.js'
+import { t } from '@/shared/i18n/i18n.js'
 
 export class SkipCommand extends BaseDiscordCommand {
   constructor(client: Atlas) {
@@ -9,6 +10,14 @@ export class SkipCommand extends BaseDiscordCommand {
 
   async run({ guild, interaction }: CommandContext) {
     if (!guild) return void interaction.reply('This command can only be used in a server.')
+
+    const userVoiceState = await guild.voiceStates.fetch(interaction.user.id)
+    if (!userVoiceState || !userVoiceState.channelId) {
+      return void interaction.reply({
+        content: t('command.play.missingVoiceChannel'),
+        flags: ['Ephemeral']
+      })
+    }
 
     const player = this.client.lavalink.players.get(guild.id)
     if (!player) {
