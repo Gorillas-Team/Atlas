91d846387317f10bb17470d0b9b655eb32c46f0e
Psykka <36780964+Psykka@users.noreply.github.com>
2025-05-15 02:50:13 -0300

    feat: enhance skip command with internationalization and user feedback

--- src/app/commands/music/skip.ts
+++ src/app/commands/music/skip.ts
@@ -5,26 +5,48 @@ import { t } from '@/shared/i18n/i18n.js'
 
 export class SkipCommand extends BaseDiscordCommand {
   constructor(client: Atlas) {
-    super(client, new SlashCommandBuilder().setName('skip').setDescription('pula'))
+    super(
+      client,
+      new SlashCommandBuilder().setName('skip').setDescription(t('command.skip.description'))
+    )
   }
 
   async run({ guild, interaction }: CommandContext) {
-    if (!guild) return void interaction.reply('This command can only be used in a server.')
+    if (!guild) return void interaction.reply(t('command.notInGuild'))
 
     const userVoiceState = await guild.voiceStates.fetch(interaction.user.id)
+    const botVoiceState = await guild.voiceStates.fetch(this.client.user!.id)
+    if (!botVoiceState || !botVoiceState.channelId) {
+      return void interaction.reply({
+        content: t('command.skip.missingBotVoiceChannel'),
+        flags: ['Ephemeral']
+      })
+    }
+
+    if (userVoiceState.channelId !== botVoiceState.channelId) {
+      return void interaction.reply({
+        content: t('command.skip.notInSameVoiceChannel'),
+        flags: ['Ephemeral']
+      })
+    }
+
     if (!userVoiceState || !userVoiceState.channelId) {
       return void interaction.reply({
-        content: t('command.play.missingVoiceChannel'),
+        content: t('command.skip.missingUserVoiceChannel'),
         flags: ['Ephemeral']
       })
     }
 
     const player = this.client.lavalink.players.get(guild.id)
     if (!player) {
-      return void interaction.reply('Player not found')
+      return void interaction.reply(t('command.skip.playerNotFound'))
+    }
+
+    if (player.queue.length === 0) {
+      return void interaction.reply(t('command.skip.noSongsInQueue'))
     }
 
     await player.stop()
-    void interaction.reply('Music successfully skipped')
+    void interaction.reply(t('command.skip.success'))
   }
 }
